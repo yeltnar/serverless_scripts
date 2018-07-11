a2b96811999a8136ef7c67f703faa6e66addefe7
Drew Brantley <brantley@us.ibm.com>
2018-07-11 18:39:46 -0500

    adding ifttt testing this with dash

---
+++ helpers/ifttt.ts
@@ -0,0 +1,46 @@
+let requestP = require("request-promise-native");
+
+const iftttConfig={
+    "first":"https://maker.ifttt.com/trigger/",
+    "second":"/with/key/bXrf4Mm5tIy0Bjis08SiYC",
+    "push_notification":"push_notification"
+}
+
+function pushNotification(msgObj){
+
+	let title;
+	let message;
+	let link;
+
+	if( typeof msgObj === "object" ){
+		title  = msgObj.title
+		message = msgObj.message
+		link    = msgObj.link
+	}else{ // assume string
+		throw "msgObj must be an object";
+	}
+
+	let url = iftttConfig.first+iftttConfig.push_notification+iftttConfig.second
+
+	let options = { 
+		method: 'POST',
+	  	url,
+	  	headers:{
+	  		"Content-Type":"application/json"
+	  	},
+	  	body: {
+	  		"value1":title,
+	  		"value2":message,
+	  		"value3":link
+	  	},
+	  	json:true
+	};
+	return requestP(options).then((results)=>{
+		console.log("results is "+results);
+	})
+
+	//return requestP(options);
+
+}
+
+export {pushNotification}
--- schedule/serverless_files/template.ts
+++ schedule/serverless_files/template.ts
@@ -1,15 +1,19 @@
 let fs = require('fs');
+import { pushNotification } from '../../helpers/ifttt';
 
-if( process.argv[2] === undefined ){
-	console.log("no args found");
-}else{
-	fs.writeFileSync("log.json",process.argv[2]);
-	console.log(process.argv[2]);
-	let data
-	try{
-		//data = JSON.parse(process.argv[2]||"{}")
-	}catch(e){
-		console.log(e);
-	}
-}
+
+pushNotification( {"title":"title","message":"message"} )
+
+// if( process.argv[2] === undefined ){
+// 	console.log("no args found");
+// }else{
+// 	fs.writeFileSync("log.json",process.argv[2]);
+// 	//console.log(process.argv[2]);
+// 	let data
+// 	try{
+// 		//data = JSON.parse(process.argv[2]||"{}")
+// 	}catch(e){
+// 		console.log(e);
+// 	}
+// }
 
