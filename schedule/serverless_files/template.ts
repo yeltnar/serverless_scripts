ebb425a3c91df3bed997e59ad323293080e60c14
Drew Brantley <brantley@us.ibm.com>
2018-07-11 19:58:44 -0500

    add link to test notification to log

--- schedule/serverless_files/template.ts
+++ schedule/serverless_files/template.ts
@@ -2,7 +2,7 @@ let fs = require('fs');
 import { pushNotification } from '../../helpers/ifttt';
 
 
-pushNotification( {"title":"title","message":"message"} )
+pushNotification( {"title":"title","message":"message", "link":"https://ws-expose.mybluemix.net/v1/get-log?token=hello"} )
 
 // if( process.argv[2] === undefined ){
 // 	console.log("no args found");
