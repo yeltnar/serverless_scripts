47382a50a7abf102e18e495f43a94ebf42f25ae1
Drew Brantley <brantley@us.ibm.com>
2018-07-17 13:51:54 -0500

    fixing refs change notification

--- schedule/serverless_files/obd/template.ts
+++ schedule/serverless_files/obd/template.ts
@@ -1,8 +1,8 @@
 let fs = require('fs');
-import { pushNotification } from '../../helpers/ifttt';
+import { pushNotification } from '../../../helpers/ifttt';
 
 
-pushNotification( {"title":"template","message":"prob for the car", "link":"https://ws-expose.mybluemix.net/v1/get-log?token=hello"} )
+pushNotification( {"title":"template","message":"for the car", "link":"https://ws-expose.mybluemix.net/v1/get-log?token=hello"} )
 
 // if( process.argv[2] === undefined ){
 // 	console.log("no args found");
