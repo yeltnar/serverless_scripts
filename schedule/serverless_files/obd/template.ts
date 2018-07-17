8655570d3bf608bf8d1be5b84f8fd6d7a817a407
Drew Brantley <brantley@us.ibm.com>
2018-07-17 13:49:08 -0500

    adding old folder

---
+++ schedule/serverless_files/obd/template.ts
@@ -0,0 +1,19 @@
+let fs = require('fs');
+import { pushNotification } from '../../helpers/ifttt';
+
+
+pushNotification( {"title":"template","message":"prob for the car", "link":"https://ws-expose.mybluemix.net/v1/get-log?token=hello"} )
+
+// if( process.argv[2] === undefined ){
+// 	console.log("no args found");
+// }else{
+// 	fs.writeFileSync("log.json",process.argv[2]);
+// 	//console.log(process.argv[2]);
+// 	let data
+// 	try{
+// 		//data = JSON.parse(process.argv[2]||"{}")
+// 	}catch(e){
+// 		console.log(e);
+// 	}
+// }
+
