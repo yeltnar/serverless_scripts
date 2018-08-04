6836893524ccfa4864dcd2d9a0741ab4bb8ba96b
Drew Brantley <brantley@us.ibm.com>
2018-08-04 09:51:45 -0500

    geofence class

--- serverless_files/geofence/geofence.ts
+++ serverless_files/geofence/geofence.ts
@@ -1,39 +1,42 @@
-let geofence_config, helpers;
+import Parser from '../../Parser.class';
 
-function geofence(parse_obj){
-    let toReturn;
-
-    toReturn = check_geofence( parse_obj.query_body.lat, parse_obj.query_body.lon );
-
-    return toReturn;
-}
-
-function check_geofence( in_lat, in_lon ){
-
-    let matched_locations = [];
-
-    for(let k in geofence_config.points){
-        let c_lat = geofence_config.points[k].lat;
-        let c_lon = geofence_config.points[k].lon;
-
-        let lat_distance = c_lat - in_lat;
-        let lon_distance = c_lon - in_lon;
-
-        let distance = Math.sqrt( Math.pow(lat_distance,2)+Math.pow(lon_distance,2) );
-
-        if( distance <= geofence_config.points[k].threashold ){
-            matched_locations.push(geofence_config.points[k].name);
-        }
+class geofence extends Parser{
+    constructor( helpers, config, parsers ){
+        super( helpers, config, parsers );
+    }
+    _shouldParse(){
+        return false;
+    }
+    _transformObj(parserObj){return parserObj;}
+    _doParse( parserObj ){
+        let toReturn;
+    
+        toReturn = this._check_geofence( parserObj.query_body.lat, parserObj.query_body.lon );
+    
+        return toReturn;
     }
 
-    return matched_locations;
+    _check_geofence( in_lat, in_lon ){
 
+        let matched_locations = [];
+
+        for(let k in this.config.points){
+            let c_lat = this.config.points[k].lat;
+            let c_lon = this.config.points[k].lon;
+
+            let lat_distance = c_lat - in_lat;
+            let lon_distance = c_lon - in_lon;
+
+            let distance = Math.sqrt( Math.pow(lat_distance,2)+Math.pow(lon_distance,2) );
+
+            if( distance <= this.config.points[k].threashold ){
+                matched_locations.push(this.config.points[k].name);
+            }
+        }
+
+        return matched_locations;
+
+    }
 }
 
-function geofence_init( helpers_local, geofence_config_local ){
-    geofence_config = geofence_config_local;
-    helpers = helpers_local;
-    return geofence;
-}
-
-export default geofence_init;
+export default geofence;
