6468ee458c9f2fe8407557309923d1efa656ca9b
Drew Brantley <brantley@us.ibm.com>
2018-08-04 09:21:48 -0500

    change weather to work with parser class

--- serverless_files/weather/weather.ts
+++ serverless_files/weather/weather.ts
@@ -1,88 +1,87 @@
+import Parser from '../../Parser.class';
 const requestP = require('request-promise-native');
 
-let helpers,parseObj,parsers,weather_config;
-
-async function main( parse_obj ){
-
-    let toReturn:any={};
-
-    if( /sun_status/.test(parse_obj.pathName) ){
-        const results = await getCurrentLocalAstronomy( parse_obj.query_body.lat, parse_obj.query_body.lon );
-
-        toReturn.sunrise = results.moon_phase.sunrise;
-        toReturn.sunset = results.moon_phase.sunset;
-        toReturn.time = results.current_observation.local_time_rfc822;
+class weatherParser extends Parser {
+    _transformObj(parserObj){
+        return parserObj;
     }
 
-    if( /sun_up_home/.test(parse_obj.pathName) ){
-        let query_body = {lat:32.917795, lon:-96.769769};
-        toReturn = await main({query_body,pathName:"/sun_up/"});
+    _shouldParse( parserObj ):boolean{
+        return /weather/.test(parserObj.pathName);
     }
-    else if( /sun_up/.test(parse_obj.pathName) ){
+    async _doParse( parserObj ){
 
-        let query_body = {lat:parse_obj.query_body.lat, lon:parse_obj.query_body.lon};
-
-        let sunsetData = await main({query_body,pathName:"/sun_status/"});
-
-        let now = new Date();
-        
-        let sunrise = new Date();
-        sunrise.setHours( sunsetData.sunrise.hour );
-        sunrise.setMinutes( sunsetData.sunrise.minute );
-        sunrise.setSeconds(0);
-        sunrise.setMilliseconds(0);
-
-        let sunset = new Date();
-        sunset.setHours( sunsetData.sunset.hour );
-        sunset.setMinutes( sunsetData.sunset.minute );
-        sunset.setSeconds(0);
-        sunset.setMilliseconds(0);
-
-        toReturn = sunrise < now && sunset > now;
-
-        if( toReturn ){
-            console.log("sun is up")
-        }else{
-            console.log("sun is down")
+        let toReturn:any={};
+    
+        if( /sun_status/.test(parserObj.pathName) ){
+            const results = await this.getCurrentLocalAstronomy( parserObj.query_body.lat, parserObj.query_body.lon );
+    
+            toReturn.sunrise = results.moon_phase.sunrise;
+            toReturn.sunset = results.moon_phase.sunset;
+            toReturn.time = results.current_observation.local_time_rfc822;
         }
+    
+        if( /sun_up_home/.test(parserObj.pathName) ){
+            let query_body = {lat:32.917795, lon:-96.769769};
+            toReturn = await this._doParse({query_body,pathName:"/sun_up/"});
+        }
+        else if( /sun_up/.test(parserObj.pathName) ){
+    
+            let query_body = {lat:parserObj.query_body.lat, lon:parserObj.query_body.lon};
+    
+            let sunsetData = await this._doParse({query_body,pathName:"/sun_status/"});
+    
+            let now = new Date();
+            
+            let sunrise = new Date();
+            sunrise.setHours( sunsetData.sunrise.hour );
+            sunrise.setMinutes( sunsetData.sunrise.minute );
+            sunrise.setSeconds(0);
+            sunrise.setMilliseconds(0);
+    
+            let sunset = new Date();
+            sunset.setHours( sunsetData.sunset.hour );
+            sunset.setMinutes( sunsetData.sunset.minute );
+            sunset.setSeconds(0);
+            sunset.setMilliseconds(0);
+    
+            toReturn = sunrise < now && sunset > now;
+    
+            if( toReturn ){
+                console.log("sun is up")
+            }else{
+                console.log("sun is down")
+            }
+        }
+    
+    
+        return toReturn;
     }
 
+    getCurrentLocalWeather(lat, lon){
 
-    return toReturn;
+        if( this.config.token===undefined ){throw "this.config.token must be defined";}
+        if( lat===undefined ){throw "lat must be defined";}
+        if( lon===undefined ){throw "lon must be defined";}
+
+        const url = `http://api.wunderground.com/api/${this.config.token}/conditions/q/${lat},${lon}.json`
+
+        return requestP(url);
+    }
+
+    getCurrentLocalAstronomy(lat, lon){
+
+        if( this.config.token===undefined ){throw "this.config.token must be defined";}
+        if( lat===undefined ){throw "lat must be defined";}
+        if( lon===undefined ){throw "lon must be defined";}
+
+        const url = `http://api.wunderground.com/api/${this.config.token}/conditions/astronomy/q/${lat},${lon}.json`;
+
+        return requestP(url).then((res)=>{
+            // if lat/lon don't have reading this will fail
+            return JSON.parse(res);
+        });
+    }
 }
 
-function weather_init( local_helpers, local_parseObj, local_parsers, local_weather_config ){
-    helpers = local_helpers;
-    parseObj = local_parseObj;
-    parsers = local_parsers;
-    weather_config = local_weather_config;
-
-    return main;
-}
-
-export default weather_init;
-
-function getCurrentLocalWeather(lat, lon){
-
-    if( weather_config.token===undefined ){throw "weather_config.token must be defined";}
-    if( lat===undefined ){throw "lat must be defined";}
-    if( lon===undefined ){throw "lon must be defined";}
-
-    const url = `http://api.wunderground.com/api/${weather_config.token}/conditions/q/${lat},${lon}.json`
-
-    return requestP(url);
-}
-
-function getCurrentLocalAstronomy(lat, lon){
-
-    if( weather_config.token===undefined ){throw "weather_config.token must be defined";}
-    if( lat===undefined ){throw "lat must be defined";}
-    if( lon===undefined ){throw "lon must be defined";}
-
-    const url = `http://api.wunderground.com/api/${weather_config.token}/conditions/astronomy/q/${lat},${lon}.json`;
-
-    return requestP(url).then((res)=>{
-        // if lat/lon don't have reading this will fail
-        return JSON.parse(res);
-    });
-}
+export default weatherParser;
