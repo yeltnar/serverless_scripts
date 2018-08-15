04554914a5fae1ae9a4446007aa62206cadfdf45
Drew Brantley <brantley@us.ibm.com>
2018-08-15 14:24:17 -0500

    listening for state change sending push

--- serverless_files/notify_leaving/notify_leaving.ts
+++ serverless_files/notify_leaving/notify_leaving.ts
@@ -6,8 +6,28 @@ let file_location = "serverless_files/notify_leaving/leaving_state.json";
 class NotifyLeaving extends HttpParser{
     constructor( name, config ){
         super( {}, name, config );
+
+        this.registerForStateChanges(this.stateChangeListener);
         
     }
+    
+    stateChangeListener(state){
+
+        // check data is present
+        if( state.obd===undefined || state.obd.previousState===undefined ){
+            return;
+        }
+
+        // check data matches desired pre conditions
+        let shouldContinue = state.obd.engine === 'on' && state.obd.previousState.engine === 'off';
+        if( !shouldContinue ){
+            return 
+        }
+
+        this.parse({pathName:"run"});
+
+    }
+
     _shouldParse(parserObj){
         return /notify_leaving/.test(parserObj.pathName);
     }
@@ -43,6 +63,11 @@ class NotifyLeaving extends HttpParser{
             if( data.leaving ){
                 toReturn = "true";
                 toReturn = await this._parse({"pathName":"remove"});
+
+                let title = "One time notification";
+                let message = "notify_leaving/run";
+
+                this.pushNotification({title, message});
             }else{
                 toReturn = "false";
             }
