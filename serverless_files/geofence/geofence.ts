e398657b5d5ec237e9594ad7e41d14f4718c2268
Drew Brantley <brantley@us.ibm.com>
2018-08-17 18:17:11 -0500

    example of using sub parser update obd to work with sub parser change correct phone wall paper parser name

--- serverless_files/geofence/geofence.ts
+++ serverless_files/geofence/geofence.ts
@@ -1,8 +1,18 @@
 import {HttpParser} from '../../HttpParser.class';
+import { ParserContainer, AbstractSubParser } from '../../parse_framework/Parser.class';
 
 class geofence extends HttpParser{
+
+    parserContainer:ParserContainer;
+
     constructor(name, config ){
+
         super( {}, name, config );
+
+        this.parserContainer = new ParserContainer();
+
+        this.parserContainer.addPrivateParser(new check_geofence_parser("check_geofence_parser", config));
+
     }
     _shouldParse(parserObj){
         return /geofence/.test(parserObj.pathName);
@@ -10,13 +20,37 @@ class geofence extends HttpParser{
     _transformObj(parserObj){return parserObj;}
     async _parse( parserObj ){
         let toReturn;
+        toReturn = await this.parserContainer.parsePrivate(parserObj);
+
+        if( typeof toReturn!=='string' && toReturn.length===1 ){
+            toReturn = toReturn[0]
+        }
+    
+        return toReturn;
+    }
+}
+
+class check_geofence_parser extends AbstractSubParser{
+    
+    constructor(name, config){
+        super({}, name, config);
+    }
+
+    _transformObj(obj){return obj;}
+
+    _shouldParse(parserObj){
+        return /get_close_locations/.test(parserObj.pathName);
+    }
+
+    async _parse(parserObj){
+        let toReturn;
     
         toReturn = this._check_geofence( parserObj.query_body.lat, parserObj.query_body.lon );
     
         return toReturn;
     }
 
-    _check_geofence( in_lat, in_lon ){
+   private  _check_geofence( in_lat, in_lon ){
 
         let matched_locations = [];
 
