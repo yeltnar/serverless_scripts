1381e233b7b0de76d223710715f4bee61c5d6d22
Drew Brantley <brantley@us.ibm.com>
2018-09-11 17:38:29 -0500

    ws_parse now uses a parser

--- ws_parser.ts
+++ ws_parser.ts
@@ -1,76 +1,92 @@
+import {init_pushNotification} from './helpers/ifttt' 
+import MyParserContainer from './MyParserContainer.class';
+import {AbstractParser} from './parse_framework/Parser.class';
+import StateLoader from './parse_framework/StateLoader.class'
+
 const config = require('config');
 const {exec} = require("child_process");
 const fs = require("fs");
 
-//my files
-import {init_pushNotification} from './helpers/ifttt' 
-//import lights from './serverless_files/lights/lights';
-
-import MyParserContainer from './MyParserContainer.class';
-
-
 let mainParserContainer = new MyParserContainer();
-
-// console.log(mainParserContainer.parseExposed)
-// process.exit()
-
-
-//const serverless_folder = config.serverless_folder; // serverless_folder has the `/` at the end
 
 let in_file_location = process.argv[2];
 let out_file_name = process.argv[3];
 let out_folder_location = process.argv[4];
+
+let deviceName = "";
+
+class MasterParser extends AbstractParser{
+
+    testRegex=/.*/;
+
+    constructor(){
+
+        super( "MasterParser", {}, new StateLoader("MasterParser", "masterParser.json"), mainParserContainer);
+    }
+
+    _abstractTransformObj( obj ){
+
+        let query_body = {};
+
+        for(let k in obj.request.query){
+            query_body[k] = obj.request.query[k];
+        }
+        for(let k in obj.request.body){
+            query_body[k] = obj.request.body[k];
+        }
+
+        return obj;
+    }
+
+    async _parse( obj ){
+
+        let result:any=[];
+
+        try{
+            result = await mainParserContainer.parseExposed(obj);
+            result = result.length===1 ? result[0] : result;
+        }catch(e){
+            console.log(e);
+            result = e;
+        }
+        
+
+        // leave at end of function 
+        if( !obj.result ){
+        }
+
+
+        writeToOutFile(result);
+    }
+}
+
+let masterParser = new MasterParser();
+let doParseObj = masterParser.parse;
 
 if( in_file_location === undefined ){
     // prob importing 
     console.log("module.parent");
     console.log(module.parent);
     //throw "file_location not defined";
+
+    deviceName = "not_provided";
+
 }else{
+    let obj:any;
 
     try{
-        let obj = JSON.parse(fs.readFileSync( in_file_location ).toString());
-        doParseObj( obj );
+        obj = JSON.parse(fs.readFileSync( in_file_location ).toString());
+
+        if( obj.response_device && obj.response_device.device_name ){
+            deviceName = obj.response_device.device_name;
+        }
+
+        masterParser.parse( obj );
     }catch(e){
         console.error(e);
     }
+
 }
-
-async function doParseObj(obj) {
-
-    let pathName = obj.request._parsedUrl.pathname;
-    let query_body = {};
-    let result:any=[];
-
-    // TODO put this in a better spot
-    if( obj.response_device && obj.response_device.device_name ){
-        init_pushNotification(obj.response_device.device_name);
-    }
-
-    for(let k in obj.request.query){
-        query_body[k] = obj.request.query[k];
-    }
-    for(let k in obj.request.body){
-        query_body[k] = obj.request.body[k];
-    }
-
-    try{
-        result = await mainParserContainer.parseExposed(obj);
-
-        result = result.length===1 ? result[0] : result;
-    }catch(e){
-        console.log(e);
-        result = e;
-    }
-
-    // leave at end of function 
-    if( !obj.result ){
-    }
-
-
-    writeToOutFile(result);
-}
-
 
 function runShell(toExec, options, params=""){
 	return new Promise((resolve, reject)=>{
