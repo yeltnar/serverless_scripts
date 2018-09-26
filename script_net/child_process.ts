8ae1f556993b02a6c3cf11a2ca3ee594ea4612c5
Drew Brantley <brantley@us.ibm.com>
2018-09-26 12:17:32 -0500

    better start up flow add child process package json loading package json to start app

--- script_net/child_process.ts
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-
-console.log("worked"); 
-
-//fs.writeFileSync("out.txt",(new Date()).toString());
--- script_net/process_manager.ts
+++ script_net/process_manager.ts
@@ -1,97 +1,180 @@
 const pm2 = require('pm2');
 const {exec} = require("child_process");
+const fs = require('fs');
 
 const noDaemonMode = false;
 
-pm2.connect(noDaemonMode, (err)=>{
-    if( err ){
-        console.error(err);
-    }else{
-        console.log("connected to pm2");
+const child_process_arr:Array<ChildProcessMetadataInterface> = [];
 
-        const arr = ["child_process"];
-        const promise_arr=[];
-
-        arr.forEach((cur, i, arr)=>{
-            const current_promise = startChildProcess( cur );
-            promise_arr.push(current_promise);
-        })
-
-        Promise.all( promise_arr ).then((resultArr)=>{
-            //pm2.disconnect();
-
-            console.log("~Start Process report~"); // buffer for better ui
-            resultArr.forEach((cur, i, arr)=>{
-                console.log(cur);
-            });
-            console.log("~Start Process Done~")
-            process.exit(1);
-        })
-    }
+Promise.all([ connectPm2(), loadChildProcessArr() ])
+.then(startChildProcesses)
+.catch((e)=>{
+    console.error("Connect to pm2 failed");
+    console.error(e);
 });
 
-async function startChildProcess( process_name ){
+function connectPm2():Promise<void>{
+    return new Promise((resolve, reject)=>{
 
-    console.log("Starting: "+process_name)
-    await compile( process_name, "build" );
-    return await run( process_name );
+        pm2.connect(noDaemonMode, (err)=>{
+            if( err ){
+                console.error(err);
+                reject();
+            }else{
+                console.log("connected to pm2"); 
+                resolve();
+            }
+        });
+    });
+}
+
+function loadChildProcessArr(){
+
+    let child_process_metadata_arr = fs.readFileSync("./child_processes.txt").toString().split("\n");
+    child_process_metadata_arr.forEach((cur, i, arr) => {
+
+        const package_location = cur+"/package.json";
+        
+        const cur_package = JSON.parse( fs.readFileSync(package_location).toString() );
+
+        let name;
+        let type;
+        let main;
+
+        if( cur_package.script_node !== undefined ){
+            name = cur_package.script_node.name || cur_package.name;
+            type = cur_package.script_node.type;
+            main = cur_package.script_node.main || cur_package.main;  
+        }else{
+            name = cur_package.name;
+            type = cur_package.type;
+            main = cur_package.main;           
+        }
+
+        const child_metadata = {
+            name,
+            pm2_name:name+i,
+            type,
+            main,
+            package_location,
+            location:cur,
+            index:i
+        };
+        
+        child_process_arr.push(child_metadata);
+        console.log( "Loaded metadata: "+JSON.stringify(child_metadata) );
+    });
+}
+
+function startChildProcesses(){
+    
+    const promise_arr=[];
+
+    child_process_arr.forEach((cur, i, arr)=>{
+        const current_promise = startChildProcess( cur );
+        promise_arr.push(current_promise);
+    })
+
+    return Promise.all( promise_arr ).then((resultArr)=>{
+        //pm2.disconnect();
+
+        console.log("~Start Process report~"); // buffer for better ui
+        resultArr.forEach((cur, i, arr)=>{
+            console.log(cur);
+        });
+        console.log("~Start Process Done~")
+        process.exit(1);
+    })
+}
+
+async function startChildProcess( process_metadata:ChildProcessMetadataInterface ){
+
+    console.log("Starting: "+process_metadata.pm2_name)
+    await compile( process_metadata );
+    return await run( process_metadata );
 
 }
 
-function compile( in_file, out_dir ):Promise<void>{
+function compile( process_metadata:ChildProcessMetadataInterface ):Promise<void>{
 
     return (new Promise(( resolve, reject )=>{
-        
-        const toExec = "tsc "+in_file+" --outDir "+out_dir;
 
-        console.log("Compiling child Process: "+toExec);
+        process_metadata.should_compile = process_metadata.type === "ts" || process_metadata.type==="typescript";
 
-        exec(toExec, undefined, (err, stdout, stderr)=>{
-            // if( err ){
-            //     reject(JSON.stringify({err,stderr}));
-            // }else{
-            //     console.log("no err");
-            //     console.log(stdout);
-            //     resolve();
-            // }
+        if( process_metadata.should_compile ){
+
+            const out_dir = process_metadata.location+"/build";
+
+            const toExec = "tsc "+process_metadata.location+"/"+process_metadata.main+" --outDir "+out_dir;
+
+            console.log("Compiling: "+process_metadata.pm2_name+": "+toExec);
+    
+            exec(toExec, undefined, (err, stdout, stderr)=>{
+                // if( err ){
+                //     reject(JSON.stringify({err,stderr}));
+                // }else{
+                //     console.log("no err");
+                //     console.log(stdout);
+                //     resolve();
+                // }
+                resolve();
+            })
+
+        }else{
             resolve();
-        })
-
+        }
     }))
     .then((value)=>{
-        console.log("\tCompile done "+in_file);
+        console.log("Compile done: "+process_metadata.pm2_name);
     })
     .catch(genericPromiseCatch);
 }
 
-function run( process_name ):Promise<{error:Boolean, process_name:string}>{
+function run( process_metadata:ChildProcessMetadataInterface ):Promise<{error:Boolean, process_name:string}>{
 
     return new Promise((resolve, reject)=>{
-        console.log("Running: "+process_name);
+        console.log("Running: "+process_metadata.pm2_name);
 
         // http://pm2.keymetrics.io/docs/usage/pm2-api/
         const options = {
-            //"name":process_name,
-            "script":"./build/"+process_name+".js",
+            "name":process_metadata.pm2_name,
+            "script":"",
             //"interpreter":"node"
-            //"args":[process_name],
+            "args":[process_metadata.index],
             //"cwd":"",
             //"force":true,
 
         };
 
+        if( process_metadata.should_compile ){
+            options.script = process_metadata.location+"/build/"+process_metadata.name+".js"
+        }else{
+            options.script = process_metadata.location+"/"+process_metadata.main;
+        }
+
         pm2.start( options, function errback(err){
 
             if( err ){
-                console.error( "Error starting: "+process_name );
+                console.error( "Error starting: "+process_metadata.pm2_name );
                 console.error( err );
-                resolve({error:true, process_name})
+                resolve({error:true, process_name:process_metadata.pm2_name})
             }else{
-                console.log("Started: "+process_name)
-                resolve({error:false, process_name});
+                console.log("Started: "+process_metadata.pm2_name)
+                resolve({error:false, process_name:process_metadata.pm2_name});
             }
         });
     });
+}
+
+interface ChildProcessMetadataInterface{
+    "type":string,
+    "name":string,
+    "pm2_name":string,
+    "main":string,
+    "should_compile"?:boolean,
+    "index":number,
+    "package_location":string,
+    "location":string
 }
 
 // move to helper area
